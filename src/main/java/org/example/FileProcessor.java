ab3c8444ec75172b7f5d94eb6601cb7bcab162be
josuevanier <150824779+josuevanier@users.noreply.github.com>
2024-02-28 11:25:51 -0500

    We must throws the IOEception into the body bame and we also throw it inside the method

---
+++ src/main/java/org/example/FileProcessor.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.io.IOException;
+
+public class FileProcessor {
+    public static void main(String[] args) throws IOException{
+        if(filepath.equals("Ivalid path")){
+            throw new IOException("Invalid path");
+        }
+    }
+}
