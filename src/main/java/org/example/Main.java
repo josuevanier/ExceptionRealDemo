90540b732a726a78cd3866b3bf65d16539f5cd5b
josuevanier <150824779+josuevanier@users.noreply.github.com>
2024-02-28 10:39:40 -0500

    Java can catch multiple exception based on the hierachy

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Opt+Enter with your caret at the highlighted text to see how
+
+        try{
+            int[] a = new int[5];
+            a[5] = 50/ 0;
+        }catch (ArithmeticException e){
+            System.out.println(e);
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println(e);
+        }catch (Exception e ) {
+            System.out.println(e);
+        }
+    }
+}
